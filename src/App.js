fe64ef9f235eea3d06572856ffa59c9094a362a8
GuntreddyHemanth <guntreddyhemanth@gmail.com>
2024-05-07 19:35:16 +0000

    savethis

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import {Component} from 'react'
 import './App.css'
 
 import Header from './components/Header'
+import GameOver from './components/GameOver'
 
 const defaultValues = {
   score: 0,
   timer: 60,
+  gameOver: true,
 }
 
 class App extends Component {
@@ -14,11 +16,36 @@ class App extends Component {
     ...defaultValues,
   }
 
+  setTimerInterval = () => {
+    this.timerId = setInterval(this.setTimer, 1000)
+  }
+
+  setTimer = () => {
+    const {timer} = this.state
+    if (timer > 0) {
+      this.setState({timer: timer - 1})
+    } else {
+      clearInterval(this.timerId)
+      this.setState({gameOver: true})
+    }
+  }
+
+  onClickGameOver = () => {
+    this.setState({...defaultValues}, this.setTimerInterval)
+  }
+
   render() {
-    const {timer, score} = this.setState
+    const {timer, score, gameOver} = this.state
     return (
       <div className="app-container">
         <Header score={score} timer={timer} />
+        <div className="game-view-container">
+          {gameOver ? (
+            <GameOver score={score} onClick={this.onClickGameOver} />
+          ) : (
+            true
+          )}
+        </div>
       </div>
     )
   }
---
+++ src/components/GameOver/index.js
@@ -0,0 +1,25 @@
+import './index.css'
+
+const GameOver = props => {
+  const {score, onClick} = props
+  return (
+    <div className="score-card">
+      <img
+        className="trophy-card"
+        src="https://assets.ccbp.in/frontend/react-js/match-game-trophy.png"
+        alt="trophy"
+      />
+      <p className="text">YOUR SCORE</p>
+      <p className="result-score">{score}</p>
+      <button className="result-button" type="button" onClick={onClick}>
+        <img
+          className="reset-image"
+          src="https://assets.ccbp.in/frontend/react-js/match-game-play-again-img.png"
+          alt="reset"
+        />
+        PLAY AGAIN
+      </button>
+    </div>
+  )
+}
+export default GameOver
